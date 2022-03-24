f79b252c27c9613b046921d64f4a4202e32381be
eye <eye@test.com>
2022-03-24 13:23:45 +0800

    update: 新增YYWebImage在解码时解密

---
+++ SDWebImageEncrypt/SDWebImageEncrypt/YYImageDecrypt/YYImageDecoder+FE.h
@@ -0,0 +1,16 @@
+//
+//  YYImageDecoder+FE.h
+//  SDWebImageEncrypt
+//
+//  Created by Flow on 3/24/22.
+//
+
+#import <YYImage/YYImage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYImageDecoder (FE)
+
+@end
+
+NS_ASSUME_NONNULL_END
